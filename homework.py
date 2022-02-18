f47110f89fcea46158139556a0d00903e52eb379
Александр Кушелев <a.kushelev@yandex.ru>
2022-02-19 00:46:42 +0400

    24

--- homework.py
+++ homework.py
@@ -15,10 +15,10 @@ class InfoMessage:
         self.speed = speed
 
     def get_message(self) -> str:
-        return (f'Тип тренировки: {self.training_type}; ',
-                f'Длительность: {self.duration:.3f} ч; ',
-                f'Дистанция: {self.distance:.3f} км; ',
-                f'Ср. скорость: {self.speed:.3f} км/ч; ',
+        return (f'Тип тренировки: {self.training_type}; '
+                f'Длительность: {self.duration:.3f} ч; '
+                f'Дистанция: {self.distance:.3f} км; '
+                f'Ср. скорость: {self.speed:.3f} км/ч; '
                 f'Потрачено ккал: {self.calories:.3f}.')
 
 
